f34ed8622b47928ccfaf090b5a58b55c14a2e24a
Guirwn <gnakamura3@gatech.edu>
2024-01-23 21:35:54 -0500

    meeing end

--- meeting24_01_23.py
+++ meeting24_01_23.py
@@ -1,3 +1,26 @@
-import matplotlib as plt
+#Info
+
+#Tasks
+#Add people to github
+#Music to code
+#Data to Analyze
+
+#Concepts
+
+#Coding
+import matplotlib.pyplot as plt
 import pandas as pd
 
+data = pd.read_csv('MechanicalPropertiesMetals.csv')
+plottingData = data.drop(['Heat treatment','G','ID','Material','Std','A5','Bhn','mu','pH','Desc','HV'], axis = 1, inplace = True) 
+su = plottingData['Su']
+sy = plottingData['Sy']
+print(su)
+print(sy)
+
+#plottingData.to_csv('SySu_Data', index = False)
+
+plt.scatter(su, sy, color = 'b')
+plt.plot([1, 1000], [1, 1000], '--')
+plt.xticks()
+plt.yticks()
