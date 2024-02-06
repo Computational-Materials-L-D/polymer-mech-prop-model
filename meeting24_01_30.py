75809d796be118b59b9f5fc474ec8223b0b814a0
Guirwn <gnakamura3@gatech.edu>
2024-02-06 18:27:23 -0500

    February 6th

--- meeting24_01_30.py
+++ meeting24_01_30.py
@@ -11,21 +11,37 @@
 import numpy as np
 import sklearn as sk
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
+from matplotlib.figure import Figure  
 import pandas as pd
 
 data = pd.read_csv('MechanicalPropertiesMetals.csv')
-plottingData = data.drop(['Heat treatment','G','ID','Material','Std','A5','Bhn','mu','pH','Desc','HV'], axis = 1, inplace = True) 
-su = plottingData['Su']
-sy = plottingData['Sy']
+
+su = data['Su']
+sy = data['Sy']
 print(su)
 print(sy)
 
+#Figure Size
+fig = plt.figure(figsize =(4, 4))
+ax = fig.add_axes([2, 2, 2, 2])
+ax = fig.add_subplot(2, 1, 1)
+ax.set_yscale('log')
+ax.set_xscale('linear')
+plt.scatter(su, sy, color = 'g', marker = 'x', s = 1, label = 'N')
+#line = ax.plot(su, color='blue', lw=2)
+
+ax = fig.add_subplot(2, 1, 2)
+ax.set_yscale('linear')
+ax.set_xscale('linear')
+plt.scatter(su, sy, color = 'g', marker = 'x', s = 1, label = 'N')
+
+
+
 #plottingData.to_csv('SySu_Data', index = False)
 
-plt.scatter(su, sy, color = 'b')
-plt.xticks()
-plt.yticks()
+#plt.xticks()
+#plt.yticks()
 
+#plt.title("Ultimate Tensile Strength vs Yield Strength") 
 
-plt.plot([1, 1000], [1, 1000], '--')
+plt.show()
---
+++ visualization.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('MechanicalPropertiesMetals.csv')
+print(data)
