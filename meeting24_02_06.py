ca134c3770709ec62eb83c96d8be4a66690a9955
Guirwn <gnakamura3@gatech.edu>
2024-02-06 19:41:19 -0500

    February 6th

--- meeting24_02_06.py
+++ meeting24_02_06.py
@@ -1,64 +1,47 @@
-#Info
-
-#Tasks
-#Add people to github
-#Music to code
-#Data to Analyze
-
-#Concepts
-
+#Survey of Matplotlib -> Pyplot
+#No new members -> Go on to regression
 #Coding
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv('MechanicalPropertiesMetals.csv')
 
-#su = data['Su']
-#sy = data['Sy']
-#e = data['E']
-#g = data['G']
-#ro = data['Ro']
-#ann = data['Heat Treatment']
+htData = data[data['Heat treatment'] != '']
 
-annData = data[data['Heat treatment'] != '']
-annE = annData['E']
-annRo = annData['Ro']
+sy = htData['Sy']
+ro = htData['Ro']
 
-#print(su)
-#print(sy)
+fig = plt.figure(figsize =(5, 5))
 
-#Figure Size
-# fig = plt.figure(figsize =(4, 4))
-# ax = fig.add_axes([2, 2, 2, 2])
-# ax = fig.add_subplot(2, 1, 1)
-# ax.set_yscale('log')
-# ax.set_xscale('linear')
-# plt.scatter(su, sy, color = 'g', marker = 'x', s = 1, label = 'N')
-#line = ax.plot(su, color='blue', lw=2)
-
-# ax = fig.add_subplot(2, 2, 1)
-# ax.set_yscale('linear')
-# ax.set_xscale('linear')
-# plt.scatter(su, sy, color = 'g', marker = 'x', s = 1, label = 'N')
-
-
-fig = plt.figure(figsize =(3, 3))
 ax = fig.add_subplot(2, 1, 1)
+ax.set_xscale('log')
 ax.set_xscale('linear')
-ax.set_xscale('linear')
-plt.scatter(annRo, annE, color = 'r', marker = '.', label = '')
-plt.title("Young Modulus vs Density") 
-plt.legend('Points Red')
+
+plt.title('Yield Strength vs Density')
+plt.legend('Red stars')
+plt.xticks()
+plt.yticks()
+plt.xlabel('Density')
+plt.ylabel('Yield Strength')
+plt.ylim((5000,6000))
+plt.grid(False)
+
+plt.scatter(sy, ro, marker = '*', color = 'r', s = 1)
 
 ax = fig.add_subplot(2, 1, 2)
 ax.set_xscale('log')
 ax.set_xscale('linear')
-plt.scatter(annRo, annE, color = 'b', marker = '*', label = '')
-plt.title("Log Young Modulus vs Density") 
-plt.legend('Stars Blue')
 
-#plottingData.to_csv('SySu_Data', index = False)
-
+plt.title('Yield Strength vs Density')
+plt.legend('Blue dots')
 plt.xticks()
 plt.yticks()
+plt.xlabel('Log Density')
+plt.ylabel('Yield Strength')
+plt.ylim((2000,5000))
+plt.grid(True)
+
+plt.scatter(sy, ro, color = 'blue', marker = '.', s = 2)
+
 plt.show()
--- visualization.py
+++ visualization.py
@@ -2,4 +2,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv('MechanicalPropertiesMetals.csv')
-print(data)
+
+htData = data[data['Heat treatment'] != '']
+
+sy = htData['Sy']
+ro = htData['Ro']
+
+fig = plt.figure(figsize =(5, 5))
+
+ax = fig.add_subplot(2, 1, 1)
+ax.set_xscale('log')
+ax.set_xscale('linear')
+# marker = '*'
+# color = 'r'
+plt.title('Yield Strength vs Density')
+plt.legend('Red stars')
+plt.xticks()
+plt.yticks()
+plt.xlabel('Density')
+plt.ylabel('Yield Strength')
+plt.ylim((5000,6000))
+plt.grid(False)
+plt.scatter(sy, ro)
+
+ax = fig.add_subplot(2, 1, 2)
+ax.set_xscale('log')
+ax.set_xscale('linear')
+# marker = '*'
+# color = 'r'
+plt.title('Yield Strength vs Density')
+plt.legend('Red stars')
+plt.xticks()
+plt.yticks()
+plt.xlabel('Log Density')
+plt.ylabel('Yield Strength')
+plt.grid(True)
+plt.ylim((2000,5000))
+plt.scatter(sy, ro)
+
+
+
+
+
+plt.show()
+
